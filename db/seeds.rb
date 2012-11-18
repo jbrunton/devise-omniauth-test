f70b8fcd5b3d2c5fc11b698498fe72333ea09e8c
John Brunton <john_brunton@hotmail.co.uk>
2012-11-18 22:42:33 +0000

    basic app as per railsapps

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'CREATING ROLES'
+
+Role.create([
+  { :name => 'admin' }, 
+  { :name => 'user' }, 
+  { :name => 'VIP' }
+], :without_protection => true)
+
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :screen_name => 'John Brunton', :email => 'john_brunton@hotmail.co.uk', :password => 'password', :password_confirmation => 'password'
+puts 'New user created: ' << user.screen_name
+user2 = User.create! :screen_name => 'Second User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
+puts 'New user created: ' << user2.screen_name
+user.add_role :admin
+user2.add_role :VIP
